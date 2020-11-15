7d03a3865443da535f649aa846ce433055596ad4
Pudding <pdng@macusers-Mac-mini.local>
2020-11-14 18:37:52 -0600

    init

---
+++ Novel CommonsTests/Novel_CommonsTests.swift
@@ -0,0 +1,33 @@
+//
+//  Novel_CommonsTests.swift
+//  Novel CommonsTests
+//
+//  Created by Pudding on 11/3/20.
+//
+
+import XCTest
+@testable import Novel_Commons
+
+class Novel_CommonsTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
